d7b7ba6b843e4e8dfe161fd834957613624df36f
Farid <faridmudzi@gmail.com>
2025-11-30 17:55:03 +0700

    Menambahkan fitur hapus dan check

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,15 @@ class FocusListApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Focus List',
-      theme: ThemeData(primarySwatch: Colors.indigo),
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.indigoAccent,
+          foregroundColor: Colors.white,
+        ),
+      ),
       home: FocusListPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -25,21 +32,31 @@ class FocusListPage extends StatefulWidget {
 }
 
 class FocusListPageState extends State<FocusListPage> {
-  final List<String> tasks = [];
+  final List<Map<String, dynamic>> tasks = [];
   final TextEditingController controller = TextEditingController();
+  final FocusNode focusNode = FocusNode();
 
   void addTask() {
     if (controller.text.isNotEmpty) {
       setState(() {
-        tasks.add(controller.text);
+        tasks.add({"text": controller.text, "done": false});
         controller.clear();
       });
+      focusNode.requestFocus();
     }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    focusNode.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[50],
       appBar: AppBar(title: Text('Focus List')),
       body: Column(
         children: [
@@ -47,20 +64,96 @@ class FocusListPageState extends State<FocusListPage> {
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               controller: controller,
+              focusNode: focusNode,
               decoration: InputDecoration(
-                labelText: 'Tambah Task',
+                hintText: 'Tambah Task',
+                hintStyle: TextStyle(color: Colors.grey[500]),
+
                 suffixIcon: IconButton(
                   icon: Icon(Icons.add),
                   onPressed: addTask,
                 ),
               ),
+              onSubmitted: (value) => addTask(),
             ),
           ),
           Expanded(
             child: ListView.builder(
               itemCount: tasks.length,
               itemBuilder: (context, index) {
-                return ListTile(title: Text(tasks[index]));
+                final task = tasks[index];
+                return Dismissible(
+                  key: Key(task["text"]),
+                  direction: DismissDirection.horizontal,
+                  confirmDismiss: (direction) async {
+                    if (direction == DismissDirection.startToEnd) {
+                      setState(() {
+                        task["done"] = true;
+                      });
+                      ScaffoldMessenger.of(context)
+                      ..clearSnackBars()
+                      ..showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'Task "${task["text"]}" marked as done',
+                          ),
+                        ),
+                      );
+                      return false;
+                    } else {
+                      final deletedTask = task["text"];
+                      setState(() {
+                        tasks.removeAt(index);
+                      });
+                      ScaffoldMessenger.of(context)
+                      ..clearSnackBars()
+                      ..showSnackBar(
+                        SnackBar(
+                          content: Text('Task "$deletedTask" deleted'),
+                        ),
+                      );
+                      return true;
+                    }
+                  },
+                  background: Container(
+                    color: Colors.lightGreen[400],
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Icon(Icons.check, color: Colors.white),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.red[400],
+                    alignment: Alignment.centerRight,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Icon(Icons.delete, color: Colors.white),
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      task["text"],
+                      style: TextStyle(
+                        decoration: task["done"]
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                        color: task["done"]
+                            ? Colors.grey
+                            : Colors.black,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete, color: Colors.grey[400]),
+                      onPressed: () {
+                        setState(() {
+                          tasks.removeAt(index);
+                        });
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Task "${tasks[index]}" deleted '),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
               },
             ),
           ),
